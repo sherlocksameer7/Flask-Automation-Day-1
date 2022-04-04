a54cebc26703a6fb2d21b7254e18d34b2712b7e0
sherlocksameer7 <arijitsameers@gmail.com>
2022-04-04 10:14:21 +0530

    Pages Concept Added

--- App.py
+++ App.py
@@ -5,8 +5,17 @@ app = Flask(__name__)
 def welcome():
     return "Hi, This is my Website"
 
-@app.route('/')
-def contact_page()
+@app.route('/contact')
+def contact_page():
+    return "Visiting Contact Page"
+
+@app.route('/home')
+def home_page():
+    return "Visiting Home Page"
+
+@app.route('/gallery')
+def gallery_page():
+    return "Visiting Gallery Page"
 
 if __name__ == "__main__":
     app.run()
